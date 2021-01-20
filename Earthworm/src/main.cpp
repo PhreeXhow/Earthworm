1b92a8a9693fee42dc4a27142b2834f8a436c56a
PhreeXhow <cnaber@boreddevelopers.com>
2021-01-19 19:54:46 -0800

    Test Net Connection with LCD

--- Earthworm/src/main.cpp
+++ Earthworm/src/main.cpp
@@ -6,6 +6,12 @@
 #include <WiFiManager.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
+#include <LiquidCrystal_PCF8574.h>
+#include <Wire.h>
+
+LiquidCrystal_PCF8574 lcd(0x27); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+int show = -1;
 
 const char* host = "Earthworm";
 
@@ -13,9 +19,23 @@ ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
 
 void setup(void) {
-
+  int error;
   Serial.begin(115200);
-  Serial.println();
+  Serial.println("Configuring LCD");
+  Wire.begin();
+  Wire.beginTransmission(0x27);
+  error = Wire.endTransmission();
+  Serial.print("Error: ");
+  Serial.print(error);
+
+  if (error == 0) {
+    Serial.println(": LCD found.");
+    show = 0;
+    lcd.begin(20, 4); // initialize the lcd
+
+  } else {
+    Serial.println(": LCD not found.");
+  }
   Serial.println("Booting Sketch...");
   WiFiManager wifiManager;
   Serial.println("Starting WiFi OR Starting WiFi Config Manager...");
@@ -32,4 +52,67 @@ void loop(void) {
   Serial.println("In main loop, doing main loop stuff...");
   httpServer.handleClient();
   MDNS.update();
+    if (show == 0) {
+    lcd.setBacklight(255);
+    lcd.home();
+    lcd.clear();
+    lcd.print("Hello LCD");
+    delay(1000);
+
+    lcd.setBacklight(0);
+    delay(400);
+    lcd.setBacklight(255);
+
+  } else if (show == 1) {
+    lcd.clear();
+    lcd.print("Cursor On");
+    lcd.cursor();
+
+  } else if (show == 2) {
+    lcd.clear();
+    lcd.print("Cursor Blink");
+    lcd.blink();
+
+  } else if (show == 3) {
+    lcd.clear();
+    lcd.print("Cursor OFF");
+    lcd.noBlink();
+    lcd.noCursor();
+
+  } else if (show == 4) {
+    lcd.clear();
+    lcd.print("Display Off");
+    lcd.noDisplay();
+
+  } else if (show == 5) {
+    lcd.clear();
+    lcd.print("Display On");
+    lcd.display();
+
+  } else if (show == 7) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("*** first line.");
+    lcd.setCursor(0, 1);
+    lcd.print("*** second line.");
+
+  } else if (show == 8) {
+    lcd.scrollDisplayLeft();
+  } else if (show == 9) {
+    lcd.scrollDisplayLeft();
+  } else if (show == 10) {
+    lcd.scrollDisplayLeft();
+  } else if (show == 11) {
+    lcd.scrollDisplayRight();
+
+  } else if (show == 12) {
+    lcd.clear();
+    lcd.print("write-");
+
+  } else if (show > 12) {
+    lcd.print(show - 13);
+  } // if
+
+  delay(1400);
+  show = (show + 1) % 16;
 }
