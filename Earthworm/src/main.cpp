b65e1e42d076e418366a007185cce64bf51b9e0a
PhreeXhow <cnaber@boreddevelopers.com>
2021-01-20 17:55:58 -0800

    Testing 595 SR

--- Earthworm/src/main.cpp
+++ Earthworm/src/main.cpp
@@ -2,40 +2,35 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <DNSServer.h>
+#include <SPI.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
-#include <LiquidCrystal_PCF8574.h>
-#include <Wire.h>
-
-LiquidCrystal_PCF8574 lcd(0x27); // set the LCD address to 0x27 for a 16 chars and 2 line display
-
-int show = -1;
 
 const char* host = "Earthworm";
+
+const int pin_ser_dat = D6;
+const int pin_ser_clk = D0;
+const int pin_reg_latch = D7;
+const int pin_mstr_rst = D8;
+
+volatile byte controlStatus = 0;
+volatile unsigned long millisfrom = 0;
 
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
 
+void sendToShiftRegister(unsigned char value) {  
+  SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+  SPI.write(value);
+  SPI.endTransaction();
+}
+
 void setup(void) {
-  int error;
+  pinMode(pin_mstr_rst,OUTPUT);
+  digitalWrite(pin_mstr_rst,LOW); 
   Serial.begin(115200);
-  Serial.println("Configuring LCD");
-  Wire.begin();
-  Wire.beginTransmission(0x27);
-  error = Wire.endTransmission();
-  Serial.print("Error: ");
-  Serial.print(error);
-
-  if (error == 0) {
-    Serial.println(": LCD found.");
-    show = 0;
-    lcd.begin(20, 4); // initialize the lcd
-
-  } else {
-    Serial.println(": LCD not found.");
-  }
   Serial.println("Booting Sketch...");
   WiFiManager wifiManager;
   Serial.println("Starting WiFi OR Starting WiFi Config Manager...");
@@ -45,74 +40,17 @@ void setup(void) {
   httpUpdater.setup(&httpServer);
   httpServer.begin();
   MDNS.addService("http", "tcp", 80);
-  Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\nThis Service allows espota to send firmware over WiFi\n\n\n", host);
+  Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n\n\n", host);
+  sendToShiftRegister(random(255));
+  millisfrom = millis();
 }
 
 void loop(void) {
   Serial.println("In main loop, doing main loop stuff...");
   httpServer.handleClient();
   MDNS.update();
-    if (show == 0) {
-    lcd.setBacklight(255);
-    lcd.home();
-    lcd.clear();
-    lcd.print("Hello LCD");
-    delay(1000);
-
-    lcd.setBacklight(0);
-    delay(400);
-    lcd.setBacklight(255);
-
-  } else if (show == 1) {
-    lcd.clear();
-    lcd.print("Cursor On");
-    lcd.cursor();
-
-  } else if (show == 2) {
-    lcd.clear();
-    lcd.print("Cursor Blink");
-    lcd.blink();
-
-  } else if (show == 3) {
-    lcd.clear();
-    lcd.print("Cursor OFF");
-    lcd.noBlink();
-    lcd.noCursor();
-
-  } else if (show == 4) {
-    lcd.clear();
-    lcd.print("Display Off");
-    lcd.noDisplay();
-
-  } else if (show == 5) {
-    lcd.clear();
-    lcd.print("Display On");
-    lcd.display();
-
-  } else if (show == 7) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("*** first line.");
-    lcd.setCursor(0, 1);
-    lcd.print("*** second line.");
-
-  } else if (show == 8) {
-    lcd.scrollDisplayLeft();
-  } else if (show == 9) {
-    lcd.scrollDisplayLeft();
-  } else if (show == 10) {
-    lcd.scrollDisplayLeft();
-  } else if (show == 11) {
-    lcd.scrollDisplayRight();
-
-  } else if (show == 12) {
-    lcd.clear();
-    lcd.print("write-");
-
-  } else if (show > 12) {
-    lcd.print(show - 13);
-  } // if
-
-  delay(1400);
-  show = (show + 1) % 16;
+  controlStatus = random(255);
+  if (millisfrom + 750 > millis()) {
+    sendToShiftRegister(controlStatus);
+  }
 }
